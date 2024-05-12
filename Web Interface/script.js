63c5f2fbd6cdd35c0b11a461807dfe85b99a7ac8
tobymac208 <niklas_fernandez@comcast.net>
2024-05-12 17:35:45 -0500

    Adds input validation for the password generation -- bounds checking mostly.

--- Web Interface/script.js
+++ Web Interface/script.js
@@ -14,6 +14,43 @@ const download = (filename, text) => {
   document.body.removeChild(element);
 };
 
+const generatePassword = () => {
+  const CHARACTERS_FOR_PASSWORD =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ1234567890$!@#";
+  const REQUIRED_LENGTH_OF_PASSWORD = 10;
+  const MAXIMUM_LENGTH_OF_PASSWORD = 40;
+
+  let length_of_password = REQUIRED_LENGTH_OF_PASSWORD;
+  let generated_password = "";
+
+  // Attempt to get the password length from the user.
+  try {
+    length_of_password = Number(
+      document.getElementById("passwordLength").value
+    );
+  } catch (e) {
+    alert(
+      `Invalid password length. Using default length of ${REQUIRED_LENGTH_OF_PASSWORD}`
+    );
+  }
+
+  if ( length_of_password > MAXIMUM_LENGTH_OF_PASSWORD || length_of_password < REQUIRED_LENGTH_OF_PASSWORD) {
+    alert(
+      `The password either too long or too short. Must be greater than ${REQUIRED_LENGTH_OF_PASSWORD} and less than or equal to the maximum of ${MAXIMUM_LENGTH_OF_PASSWORD}. Defaulting to ${REQUIRED_LENGTH_OF_PASSWORD}.`
+    );
+    length_of_password = REQUIRED_LENGTH_OF_PASSWORD;
+  }
+
+  for (let i = 0; i < length_of_password; i++) {
+    generated_password +=
+      CHARACTERS_FOR_PASSWORD[Math.floor(Math.random() * CHARACTERS_FOR_PASSWORD.length)];
+  }
+
+  // Upate the elements on the page with the new password.
+  document.getElementById("password").value = generated_password;
+  document.getElementById("passwordVerify").value = generated_password;
+};
+
 const createIdentity = (event) => {
   event.preventDefault();
 
@@ -25,7 +62,7 @@ const createIdentity = (event) => {
   let dob = document.getElementById("dob").value;
   let pin = document.getElementById("pin").value;
 
-  const PIN_MIN_LENGTH = 4
+  const PIN_MIN_LENGTH = 4;
 
   if (password !== passwordVerify) {
     alert("Passwords do not match!");
@@ -38,7 +75,19 @@ const createIdentity = (event) => {
   }
 
   // Collect all of the information and send it over to the textarea element on the page. Allows the user to save all of the information.
-  let identityInformation = "Name: " + firstName + " " + lastName + "\nEmail: " + email + "\nPassword: " + password + "\nDate of birth: " + dob + "\nPIN: " + pin;
+  let identityInformation =
+    "Name: " +
+    firstName +
+    " " +
+    lastName +
+    "\nEmail: " +
+    email +
+    "\nPassword: " +
+    password +
+    "\nDate of birth: " +
+    dob +
+    "\nPIN: " +
+    pin;
   document.getElementById("identityInformation").value = identityInformation; // Populates the field on the page.
   document.getElementById("filename").value = firstName[0] + lastName + ".txt"; // Generates a filename
 
