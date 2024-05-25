224d55d2263d7515473010cf7a5292fe7b6fb720
tobymac208 <niklas_fernandez@comcast.net>
2024-05-25 18:18:49 -0500

    Creates a Python script that prints out all of the users created. Also, creates a bash file that just runs the Python script to quickly view created users.

---
+++ displayusers.py
@@ -0,0 +1,23 @@
+import os
+
+
+def main():
+    all_data_concat = ""
+
+    FILE_PATH = "./Identities/"
+    file_list = os.listdir(FILE_PATH)
+
+    for item in file_list:
+        # Attempt to read each file.
+        with open(f"{FILE_PATH}{item}", "r") as f:
+            print(f"Data found in {item} >>>")
+            
+            for line in f.readlines():
+                if line != "\n":
+                    print(line.strip())
+            print("")
+
+
+if __name__ == "__main__":
+    main()
+
